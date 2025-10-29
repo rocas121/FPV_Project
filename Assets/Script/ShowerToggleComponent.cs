39199eb520818c9b2f837061134ed093ecacf279
Romaric Zhang <113245424+rocas121@users.noreply.github.com>
2025-10-29 12:02:50 +0100

    feat(scene): UI

--- Assets/Script/ShowerToggleComponent.cs
+++ Assets/Script/ShowerToggleComponent.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class ShowerToggleComponent : MonoBehaviour
 {
+    public Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
